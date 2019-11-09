3f42976f1415f285f7ef2c57e99d3d59e511f690
QADEER <qadeershahbaz@gmail.com>
2019-11-09 19:05:38 +0530

    Moved data to cntrollers for about and location details page

--- app_server/controller/about.js
+++ app_server/controller/about.js
@@ -1,7 +1,25 @@
 const about = function (req, res, next) {
-    res.render('about', { title: 'About' })
+    res.render('about',
+        {
+            title: 'Abouts',
+            content: {
+                paragraph1: " Loc8r was created to help people find places to sit down and get a piece of work done",
+                paragraph2: `As the world's largest corporate university, the Loc8r global education centre in the
+                337 acre[42] campus has 400 instructors and 200+ classrooms,[43] with international benchmarks at its
+                core. Established in 2002, it had trained around 1,25,000 engineering graduates by June 2015.[43] It can
+                train 14,000 employees at a given point of time on various technologies.[44][45]
+
+                The Loc8r Leadership Institute (ILI), based in Mysuru, has 96 rooms and trains about 400 Loc8r customers
+                annually.[45] Its purpose is to prepare and develop the senior leaders in Loc8r for current and future
+                executive leadership roles.`,
+                conclusion: ` The Loc8r Training Centre in Mysuru also provides a number of extracurricular
+                facilities like tennis, badminton, basketball, swimming pool and gym.`
+            }
+
+        }
+    )
 }
 
-module.exports= {
+module.exports = {
     about
 }
--- app_server/controller/locations.js
+++ app_server/controller/locations.js
@@ -4,7 +4,44 @@ const home = function (req, res, next) {
 
 
 const locationDetail = function (req, res, next) {
-    res.render('location-detail', { title: 'Location details' })
+    res.render('location-detail',
+        {
+            title: 'detailed Review',
+            info: {
+                id: 1,
+                name: "StarCups",
+                max_rating: 5,
+                loc_rating: 3,
+                address: "125 High Street ,Reading, RG6 IPS",
+                openingHours: ["Monday-Friday :7:00am -7:00pm ", " Saturday :8:00am -5:00pm ", "Sunday :Closed "],
+                facilities: ["Hot Drinks", "Food", "Premium Wifi"],
+                loc_url: "/images/staticmap.png",
+                cutomer_reviews: [
+                    {
+                        customer_id: 1,
+                        customer_rating: 4,
+                        customer_name: "Simon Holmes",
+                        date: "16 July 2013",
+                        customer_review: `What a great place. I can't say enough good place about
+                        it.`
+
+                    },
+                    {
+                        customer_id: 2,
+                        customer_rating: 3,
+                        customer_name: "M.A.Qadeer",
+                        date: "17 July 2019",
+                        customer_review: `Great ambience`
+
+                    }
+                ],
+                loc_overview:` Simon's cafe is on Loc8r because it has accesible wifi and a space to sitdown with ypur laptop
+                and get some work done.`
+            }
+
+
+        }
+    )
 }
 
 const locationReviewForm = function (req, res, next) {
